ed2e5eb44b4fb4eca8dd21fbcd247c6b828ce488
hari139007 <87061286+hari139007@users.noreply.github.com>
2021-09-04 18:40:00 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class Multiply_Two_num
+{
+public static void main(String args[]){
+int num1=34,num2=45,product;
+product=num1*num2;
+System.out.println("The product of given two numbers is: "+product);
+
+}
+
+}
